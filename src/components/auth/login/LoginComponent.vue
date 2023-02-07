98abb7f22d491672b9b4a05c75a92468b6c732f2
Joel Campos <joelcamposoliva9000@gmail.com>
2023-02-07 18:20:37 +0100

    Base for login form

---
+++ src/components/auth/login/LoginComponent.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { defineComponent } from '@vue/runtime-dom';
+</script>
+<template>
+    <div class="login-page">
+        <a-card title="Login form" hoverable></a-card>
+    </div>
+</template>
+
+<style lang="scss">
+    div.login-page {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
+
+<script lang="ts">
+    export default defineComponent({
+        
+    })
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,32 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+const loginState = reactive({});
+
+const isLoggedIn = true;
+</script>
+
 <template>
   <div class="home">
-    <a-button type="primary">a</a-button>
+    <LoginComponent v-if="isLoggedIn"/>
+    <a-button type="primary" v-else>a</a-button>
   </div>
 </template>
 
+<style lang="scss">
+  div.home {
+    height: 100vh;
+    width: 100vw;
+    background-color: rgb(207, 207, 207);
+  }
+</style>
+
 <script lang="ts">
 import { defineComponent } from 'vue';
+import LoginComponent from '../components/auth/login/LoginComponent.vue';
 
 export default defineComponent({
-  name: 'HomeView',
+    name: "HomeView",
+    components: { LoginComponent }
 });
 </script>
