a78027967d44d2110c5d303f6d375909da2aee9f
Joel Campos <joelcamposoliva9000@gmail.com>
2023-02-10 17:38:49 +0100

    Form

--- src/components/app/students/AddStudentForm.vue
+++ src/components/app/students/AddStudentForm.vue
@@ -1,12 +1,14 @@
 <script lang="ts" setup>
 import { PlusOutlined } from "@ant-design/icons-vue";
-import { defineComponent, ref } from "@vue/runtime-dom";
+import { ref } from "@vue/runtime-dom";
 import { StudentModel } from "../../../models/students/student.model";
 
 const newStudentFormState = ref<StudentModel>();
 const errorCode = ref<string>();
 
-const onSubmit = () => {
+const onSubmit = (values: StudentModel) => {
+  console.log(values);
+  errorCode.value = undefined;
   if (!newStudentFormState.value) return;
   const { name } = newStudentFormState.value;
   if (name.length < 2 || name.length > 64) {
@@ -19,10 +21,18 @@ const onSubmit = () => {
 </script>
 
 <template>
-  <a-form :model="newStudentFormState" @submit="onSubmit" autocomplete="off">
+  <a-form
+    :model="newStudentFormState"
+    @finish="onSubmit"
+    autocomplete="off"
+    layout="vertical"
+  >
     <a-row :gutter="[6, 6]">
       <a-col span="24">
-        <a-form-item name="name">
+        <a-form-item
+          name="name"
+          :label="$t('studentsDrawer.addForm.fields.name.Label')"
+        >
           <a-input showCount allowClear />
         </a-form-item>
       </a-col>
@@ -36,15 +46,13 @@ const onSubmit = () => {
       </a-col>
       <a-col span="24">
         <a-alert
-          :v-if="errorCode"
+          v-if="errorCode"
           :title="$t('errors.Title')"
           :message="$t(`studentsDrawer.addForm.errors.${errorCode}.Text`)"
+          type="error"
+          showIcon
         />
       </a-col>
     </a-row>
   </a-form>
 </template>
-
-<script lang="ts">
-export default defineComponent({ components: { PlusOutlined } });
-</script>
