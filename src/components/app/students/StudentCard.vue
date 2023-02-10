183c315e9f65913a6a06cec749faf38edc35bb64
Joel Campos <joelcamposoliva9000@gmail.com>
2023-02-10 18:43:04 +0100

    Update name

---
+++ src/components/app/students/StudentCard.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+import { SaveOutlined } from "@ant-design/icons-vue";
+import { doc, getFirestore, updateDoc } from "@firebase/firestore";
+import { defineProps, ref } from "vue";
+import { StudentModel } from "../../../models/students/student.model";
+import { FirebaseDocModel } from "../../../models/utils/firebase/firebase-doc.model";
+import { ColorHelper } from "../../../services/helpers/color.helper";
+
+type Props = {
+  student: FirebaseDocModel<StudentModel>;
+};
+const props = defineProps<Props>();
+
+const name = ref<string>(props.student.data.name);
+
+const onSave = async () => {
+  await updateDoc(doc(getFirestore(), "students", props.student.id), {
+    name: name.value,
+  });
+};
+</script>
+
+<template>
+  <a-card hoverable>
+    <a-row :gutter="[12, 6]">
+      <a-col span="4">
+        <a-avatar
+          :style="{
+            'background-color': ColorHelper.generateColorFromString(
+              props.student.id
+            ),
+          }"
+          >{{ props.student.data.name.substring(0, 1) }}</a-avatar
+        >
+      </a-col>
+      <a-col span="16"> <a-input v-model:value="name" /></a-col>
+      <a-col span="4">
+        <a-button type="primary" block @click="onSave">
+          <template #icon>
+            <SaveOutlined />
+          </template>
+        </a-button>
+      </a-col>
+    </a-row>
+  </a-card>
+</template>
--- src/components/app/students/StudentsList.vue
+++ src/components/app/students/StudentsList.vue
@@ -1,6 +1,5 @@
 <script lang="ts" setup>
 const studentsState = ref<FirebaseDocModel<StudentModel>[]>();
-console.log(studentsState);
 
 StudentsService.onStudentsChange((data) => (studentsState.value = data));
 </script>
@@ -9,25 +8,11 @@ StudentsService.onStudentsChange((data) => (studentsState.value = data));
   <a-row :gutter="[12, 12]">
     <a-col
       :v-if="studentsState"
-      v-for="{ data: { name }, id } of studentsState"
-      :key="id"
+      v-for="student of studentsState"
+      :key="student.id"
       span="24"
     >
-      <a-card hoverable>
-        <a-row :gutter="[12, 6]">
-          <a-col>
-            <a-avatar
-              :style="{
-                'background-color': ColorHelper.generateColorFromString(id),
-              }"
-              >{{ name.substring(0, 1) }}</a-avatar
-            >
-          </a-col>
-          <a-col>
-            <p>{{ name }}</p>
-          </a-col>
-        </a-row>
-      </a-card>
+      <StudentCard :student="student" />
     </a-col>
   </a-row>
 </template>
@@ -37,7 +22,7 @@ import { defineComponent, ref } from "vue";
 import { StudentModel } from "../../../models/students/student.model";
 import { FirebaseDocModel } from "../../../models/utils/firebase/firebase-doc.model";
 import { StudentsService } from "../../../services/app/students/students.service";
-import { ColorHelper } from "../../../services/helpers/color.helper";
+import StudentCard from "./StudentCard.vue";
 
-export default defineComponent({});
+export default defineComponent({ components: { StudentCard } });
 </script>
