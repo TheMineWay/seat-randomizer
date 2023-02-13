34d92babdf1c2d87f469199f3ffbf624da74a833
Joel Campos <joelcamposoliva9000@gmail.com>
2023-02-13 23:08:41 +0100

    Disable unauthorized logins

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,30 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import { getAuth, onAuthStateChanged, signOut, User } from "firebase/auth";
 
 const auth = getAuth();
 
 let loginState = ref<{ user?: User | null }>({ user: null });
 
-onAuthStateChanged(auth, (user) => {
-  if (user) loginState.value = { user };
-  else loginState.value = {};
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    try {
+      if (
+        (
+          (await getDoc(doc(getFirestore(), "users", user.uid))).data() as {
+            enabled: boolean;
+          }
+        ).enabled
+      ) {
+        loginState.value = { user };
+      } else {
+        signOut(auth);
+      }
+    } catch (e) {
+      console.error(e);
+      signOut(auth);
+    }
+  } else loginState.value = {};
 });
 </script>
 
@@ -34,6 +50,7 @@ import { defineComponent } from "vue";
 import LoginComponent from "../components/auth/login/LoginComponent.vue";
 import LayoutComponent from "../components/layout/LayoutComponent.vue";
 import ClassroomView from "../views/app/ClassroomView.vue";
+import { doc, getDoc, getFirestore } from "@firebase/firestore";
 
 export default defineComponent({
   name: "HomeView",
