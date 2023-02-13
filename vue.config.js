dd7420316280d260551ad14933e4f66fd2f6c969
Joel Campos <joelcamposoliva9000@gmail.com>
2023-02-13 23:39:27 +0100

    PWA config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: 'Seat randomizer',
+    themeColor: '#1890ff',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  }
 })
